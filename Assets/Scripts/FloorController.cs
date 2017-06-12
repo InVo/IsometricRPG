05cf44eac153d02c8ac2e3c9b8b8dec40d45754a
InVo <szenckov@gmail.com>
2017-06-12 17:40:37 +0300

    Isometric RPG first commit

---
+++ Assets/Scripts/FloorController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorController : MonoBehaviour {
+
+    private void OnMouseOver() {
+        if (Input.GetMouseButtonDown(1)) {
+            int c = 0;
+        }
+        if (Input.GetMouseButtonDown(0)) {
+            int c = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+    [SerializeField]
+    private Animator _animator;
+
+    private void Awake() {
+        _animator = GetComponent<Animator>();
+    }
+
+    
+}
